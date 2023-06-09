a11f35f0263f8b75760ddeb67a93a491fd04e1a4
Brodie Ashcraft <brodiea19@gmail.com>
2023-06-08 18:20:32 -0700

    complete simple fetch lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [dogPic, setDogPic] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((r) => r.json())
+      .then((doggyPic) => {
+        setDogPic(doggyPic.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return <img src={dogPic} alt="A Random Dog"></img>;
+}
+
+export default App;
